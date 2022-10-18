8e53571f67cdbc390c0be32b1e6015c50d49e89f
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-10-18 21:39:00 +0900

    Time: 685 ms (92.51%), Space: 30.8 MB (15.84%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,17 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        ans = []
+        dp = {}
+        def helper(start, temp):
+            if start == len(s):
+                ans.append(temp)
+            for end in range(start+1, len(s)+1):
+                st = s[start:end]
+
+                if st == st[::-1]:
+                    temp.append(st)
+                    helper(end, [*temp])
+                    temp.pop()
+        helper(0, [])       
+        return ans
