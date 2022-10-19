e671f51dd8888d38ab3a15a8be4470df778d1d41
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-10-20 07:06:40 +0900

    Time: 1940 ms (38.92%), Space: 75.1 MB (80.08%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,30 +1,59 @@
-class LRUCache:
+class LinkedList:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
 
+class LRUCache:
     def __init__(self, capacity: int):
         self.m = {}
-        self.d = deque()
         self.capacity = capacity
+        self.head = LinkedList(0, 0)
+        self.tail = LinkedList(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
         if key not in self.m:
             return -1
         else:
-            del self.d[self.d.index(key)]
-            self.d.append(key)
-            return self.m[key]
+            node = self.m[key]
+            self.remove(node)
+            self.add(node)
+            return node.val
+            
         
     def put(self, key: int, value: int) -> None:
             if key not in self.m:
                 if len(self.m) >= self.capacity:
-                    temp = self.d.popleft()
-                    self.m.pop(temp)
-                self.d.append(key)
-                self.m[key] = value
+                    self.removeFromHead()
+                node = LinkedList(key, value)
+                self.add(node)
+                self.m[key] = node
             else:
-                del self.d[self.d.index(key)]
-                self.d.append(key)
-                self.m[key] = value
+                self.remove(self.m[key])
+                self.m[key].val = value
+                self.add(self.m[key])
 
+    def remove(self,node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        
+    def add(self,node):
+        self.tail.prev.next = node
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev = node
+        
+        
+    def removeFromHead(self):
+        self.m.pop(self.head.next.key)
+        self.head.next = self.head.next.next
+        self.head.next.prev = self.head
+        
+        
+        
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
