f5fb3f246638f6807b9bd53477b908f5bad9d932
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-10-19 23:30:40 +0900

    Time: 6421 ms (5.00%), Space: 73.8 MB (98.88%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -13,21 +13,12 @@ class LRUCache:
             self.d.append(key)
             return self.m[key]
         
-
     def put(self, key: int, value: int) -> None:
-        if len(self.m) < self.capacity:
             if key not in self.m:
+                if len(self.m) >= self.capacity:
+                    temp = self.d.popleft()
+                    self.m.pop(temp)
                 self.d.append(key)
-                self.m[key] = value
-            else:
-                del self.d[self.d.index(key)]
-                self.d.append(key)
-                self.m[key] = value
-        else:
-            if key not in self.m:
-                temp = self.d.popleft()
-                self.d.append(key)
-                self.m.pop(temp)
                 self.m[key] = value
             else:
                 del self.d[self.d.index(key)]
