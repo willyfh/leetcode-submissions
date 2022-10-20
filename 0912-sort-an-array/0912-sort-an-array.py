691a3c7d651f8f08fcf36c10ddc8e2786a2b9e1c
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-10-20 17:02:56 +0900

    Time: 2942 ms (46.13%), Space: 21.7 MB (70.14%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,34 +1,40 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        self.mergeSort(nums)
-        return nums
-
-    def mergeSort(self, nums): 
-        if len(nums) > 1: 
-            mid = len(nums)//2
-            L = nums[:mid] 
-            R = nums[mid:] 
-
-            self.mergeSort(L)
-            self.mergeSort(R)
-
-            i = j = k = 0
-
-            while i < len(L) and j < len(R): 
-                if L[i] < R[j]: 
-                    nums[k] = L[i] 
+        
+        
+        def mergesort(nums):
+            if len(nums)>1 :
+                
+                mid = len(nums) //2
+                
+                nl = nums[:mid]
+                nr = nums[mid:]
+                
+                mergesort(nl)
+                mergesort(nr)
+                
+                
+                i = 0
+                j = 0
+                k = 0
+                while i<len(nl) and j < len(nr):
+                    if nl[i] < nr[j]:
+                        nums[k] = nl[i]
+                        i+=1
+                    else:
+                        nums[k] = nr[j]
+                        j+=1
+                    k+=1
+                    
+                while i<len(nl):
+                    nums[k] = nl[i]
                     i+=1
-                else: 
-                    nums[k] = R[j] 
+                    k+=1
+                
+                while j<len(nr):
+                    nums[k] = nr[j]
                     j+=1
-                k+=1
- 
-            while i < len(L): 
-                nums[k] = L[i] 
-                i+=1
-                k+=1
-
-            while j < len(R): 
-                nums[k] = R[j] 
-                j+=1
-                k+=1
+                    k+=1
+                    
+        mergesort(nums)
+        return nums
