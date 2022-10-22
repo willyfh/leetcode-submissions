3f5c0a03e29bd97fc2a991cfa6d51bf47807fb2a
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-10-22 21:41:12 +0900

    Time: 242 ms (6.00%), Space: 14.5 MB (38.58%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -6,15 +6,11 @@ class Solution:
         ans = dp[0][0]
         for i in range(1, len(nums)):
             
-            temp1 = dp[i-1][0]*nums[i]
-            temp2 = dp[i-1][1]*nums[i]
-            
-            a = max(temp2, max(nums[i], temp1))
-            b = min(temp1, min(nums[i], temp2))
-            
-            dp[i][0] = a
-            dp[i][1] = b
-            ans = max(ans, a)
+            candidates = [nums[i], dp[i-1][0]*nums[i], dp[i-1][1]*nums[i]]
+                        
+            dp[i][0] = max(candidates)
+            dp[i][1] = min(candidates)
+            ans = max(ans, dp[i][0])
 
         return ans
                 
