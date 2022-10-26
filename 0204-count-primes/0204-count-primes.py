85c95be1e30a5f16d26d53ed1d9511ecf999b265
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-10-26 23:03:43 +0900

    Time: 6265 ms (44.23%), Space: 70.7 MB (46.49%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,21 @@
+import numpy
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <=2:
+            return 0
+        arr = [True]*n
+        arr[0] = False
+        arr[1] = False
+        
+        i = 2
+        for i in range(2, n):
+            if not arr[i]:
+                continue
+            
+            arr[i] = True
+            for j in range(i*i, n, i):
+                    arr[j] = False
+                
+
+        return sum(arr)
+            
