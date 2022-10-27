9e755beac8f1fa8ee3638e3a893a200e93860afc
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-10-27 14:15:58 +0900

    Time: 215 ms (49.63%), Space: 17.6 MB (6.79%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,8 +1,6 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
-        
-        
+                
         def helper(i, s):
             if v[i]:
                 return True
@@ -17,25 +15,21 @@ class Solution:
             for x in d[prerequisites[i][1]]:
                 if not helper(x, s):
                     return False
+                
             v[i] = True
             return True     
             
-        
         d = {}
         
         for i in range(len(prerequisites)):
             if prerequisites[i][0] not in d:
-                
                 d[prerequisites[i][0]] = []
-                
             d[prerequisites[i][0]].append(i)
         
         v = [False] * len(prerequisites)
         
-        
         for i in range(len(prerequisites)):
             if not helper(i, set()):
                 return False
-            
             
         return True
