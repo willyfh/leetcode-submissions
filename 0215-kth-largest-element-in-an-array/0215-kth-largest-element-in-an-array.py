0942d5edfe3cafde9a452c60220addd59f1a31fb
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-10-31 11:15:45 +0900

    Time: 1361 ms (44.80%), Space: 27.2 MB (64.19%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,12 +1,10 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         
-        def helper(left, right):
             
+        def partition(left, right):
             pivot = right
-            temp_left = left
-            
-            right-=1
+            right -= 1
             while left<=right:
                 while nums[left] < nums[pivot]:
                     left += 1
@@ -21,12 +19,17 @@ class Solution:
                 right-=1
                 
             nums[left], nums[pivot] = nums[pivot], nums[left]
-            
-            if left == (len(nums) - k):
-                return nums[left]
-            elif left < (len(nums) - k):
-                return helper(left+1, pivot)
-            else: # left > (len(nums) - k)
-                return helper(temp_left, left-1)
-            
-        return helper(0, len(nums)-1)
+            return left
+        
+        left = 0
+        right = len(nums) -1
+        
+        while left < right:
+            p = partition(left, right)
+            if p == (len(nums) - k):
+                return nums[p]
+            elif p < (len(nums) - k):
+                left = p + 1
+            else: # p > (len(nums) - k)
+                right = p -1
+        return nums[left]
