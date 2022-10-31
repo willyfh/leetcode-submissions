b1a105a173cb71047a4cc75c8c7b6771abe3c778
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-10-31 12:05:07 +0900

    Time: 261 ms (22.99%), Space: 21.7 MB (5.03%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -0,0 +1,44 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        d = deque()
+        
+        
+        arr = []
+        
+        temp = ""
+        for c in s:
+            if c == " ":
+                continue
+            elif c == "+" or c == "/" or c == "*" or c == "-":
+                arr.append(temp)
+                temp = ""
+                arr.append(c)
+            else:
+                temp+=c
+                
+        arr.append(temp)
+        i = 0
+        while i < (len(arr)):
+            c = arr[i]
+
+            if c == "*" or c == "/":
+                l = d.pop()
+                i+=1
+                if c == "*":
+                    d.append(str(int(l)  * int(arr[i])))
+                else:
+                    d.append(str(int(int(l)  / int(arr[i]))))
+            else:
+                d.append(c)
+            i+=1
+        print(d)
+        while len(d)>1:
+            r = d.popleft()
+            o = d.popleft()
+            l = d.popleft()
+            if o =="+":
+                d.appendleft(str(int(r) + int(l)))
+            else:
+                d.appendleft(str(int(r) - int(l)))
+                
+        return int(d[0])
