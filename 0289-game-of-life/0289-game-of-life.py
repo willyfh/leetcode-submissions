413104390a5d56f1cfd58bf2375f8cd51694adfe
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-11-04 19:39:56 +0900

    Time: 80 ms (6.62%), Space: 13.8 MB (99.85%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -11,26 +11,27 @@ class Solution:
                     if i>=0 and i<len(board) and j>=0 and j <len(board[0]):
                         if  not (i==x and j==y):
                             c+=board[i][j]       
-                        
-            return c
+            temp[x][y] = c            
+    
         
         temp = [[0 for i in range(len(board[0]))] for j in range(len(board))]
         
         
         for i in range(len(board)):
             for j in range(len(board[0])):
-                
-                c = countNeigh(i,j)
-                if c<2:
-                    temp[i][j] = 0
-                elif c==3:
-                    temp[i][j] = 1
-                elif c>3:
-                    temp[i][j] = 0
-                else:
-                    temp[i][j] = board[i][j]
-                
-                
+                countNeigh(i,j)
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
-                board[i][j] = temp[i][j]
+                
+                c = temp[i][j]
+                if c<2:
+                    board[i][j] = 0
+                elif c==3:
+                    board[i][j] = 1
+                elif c>3:
+                    board[i][j] = 0
+                else:
+                    board[i][j] = board[i][j]
+                
+                
