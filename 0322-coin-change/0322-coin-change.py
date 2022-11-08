65ba766f7938907f4914d54bbb23e71326944702
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-11-08 10:31:01 +0900

    Time: 1266 ms (96.89%), Space: 14 MB (97.31%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,29 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        
+        q = deque()
+        
+        visited = [False]*(amount+1)
+        visited[True] # for zero value
+        
+        for c in coins:
+            if c <= amount:
+                q.append((c,1))
+        
+           
+        while len(q)>0:
+            curr, lvl = q.popleft()
+                    
+
+            if curr==amount:
+                return lvl
+                
+            for c in coins:
+                
+                t = c + curr
+                if t <= amount and not visited[t]:
+                    q.append((t, lvl+1))
+                    visited[t] = True    
+        return -1
