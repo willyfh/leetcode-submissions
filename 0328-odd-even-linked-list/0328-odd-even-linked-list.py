9ef0c0ae4d8b57493dac0841408de65d8400ea42
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-11-09 14:26:01 +0900

    Time: 83 ms (60.23%), Space: 16.6 MB (78.34%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -13,13 +13,11 @@ class Solution:
         even = head.next
         curr = odd
         curr2 = even
-        while curr.next!=None and curr2.next!=None:
+        while curr2!=None and curr2.next!=None:
             curr.next = curr.next.next
             curr2.next = curr2.next.next
             
             curr2 = curr2.next
-            if curr.next == None:
-                break
             curr = curr.next
                         
         curr.next = even
