80beb8831022c5883d67643d283da83fc9a5e69d
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-11-09 20:31:14 +0900

    Time: 2801 ms (5.37%), Space: 29.2 MB (9.33%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,16 +1,18 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         
-        temp = deque() # to store tripplet
-        temp.append(nums[0])
+        first = None
+        second = nums[0]
         for i in range(1, len(nums)):
-            if nums[i]>temp[-1]:
-                temp.append(nums[i])
-                if len(temp)==3:
+            
+            if nums[i]>second:
+                if first!=None:
                     return True
-            elif nums[i] < temp[-1]:
-                    if nums[i] > temp[0]:
-                        temp[-1] = nums[i]
+                first = second
+                second=nums[i]
+            elif nums[i] < second:
+                    if first==None or nums[i] > first:
+                        second = nums[i]
                     else:
-                        temp[0] = nums[i]
+                        first = nums[i]
         return False
