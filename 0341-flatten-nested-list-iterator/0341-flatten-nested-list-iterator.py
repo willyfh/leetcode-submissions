63979a0881d3f23019d30ea126f3e4096d14048c
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-11-09 21:30:48 +0900

    Time: 72 ms (88.00%), Space: 17.8 MB (87.04%) - LeetHub

---
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.py
@@ -0,0 +1,47 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self) -> [NestedInteger]:
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.ret = None
+        self.nestedList = deque(nestedList)
+    
+    def next(self) -> int:
+        return self.ret
+        
+    def hasNext(self) -> bool:
+        if len(self.nestedList)==0:
+            return False
+        p = self.nestedList.popleft()
+        while not p.isInteger():
+            l = p.getList()
+            for i in range(len(l)-1, -1, -1):
+                self.nestedList.appendleft(l[i])
+            if len(self.nestedList)==0:
+                return False
+            p = self.nestedList.popleft()
+        self.ret = p
+        return True
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
