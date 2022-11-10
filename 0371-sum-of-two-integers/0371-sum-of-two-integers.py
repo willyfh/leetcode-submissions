83dace7f810103eb2e789d506a8ec5bfb7a2c686
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-11-10 12:19:00 +0900

    Time: 293 ms (5.83%), Space: 31.7 MB (15.41%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,29 +1,28 @@
 import numpy
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        
         ans = 0
-        remainder = 0
+        carry = 0
         for i in range(32):
             r = 1 << i
             if a & r and b & r:
-                if remainder == 1:
+                if carry == 1:
                     ans = ans  | r
                 else:
                     ans = ans & ~r
-                    remainder = 1
+                    carry = 1
             elif a & r or b & r:
-                if remainder == 1:
+                if carry == 1:
                     ans = ans & ~r
                 else:
                     ans = ans | r
-            else: # both zeros
-                if remainder == 1:
+            else:
+                if carry == 1:
                     ans = ans | r
-                    remainder = 0
+                    carry = 0
                 else:
                     ans = ans & ~r
         
-        return numpy.int32(ans)
+        return numpy.int16(ans)
                     
              
