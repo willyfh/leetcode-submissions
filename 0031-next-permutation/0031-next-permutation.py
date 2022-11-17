46d1931ec038cefded077f276136a936c00e0e3e
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-11-17 10:14:42 +0900

    Time: 44 ms (92.98%), Space: 13.9 MB (75.26%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -4,14 +4,13 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         j = len(nums)-1
+        m = [(nums[j],j)]        
+        t = 101 # the largest value is 100
         
-        m = [(nums[j],j)]
-        
-        d = {}
-        
-        t = 101
         while j>0:
+            # find min(nums[x]) such that x > y and a[x] > a[y], then swap
             for k in m:
+
                 if k[0]>nums[j-1]:
                     if  k[0] < t:
                         t = k[0]
@@ -22,10 +21,9 @@ class Solution:
                 break
             else:
                 m.append((nums[j-1], j-1))
-            
-                
             j-=1
-            
+        
+        # sort the remaining array, here I just use a simple sort algorithm with N^2 time complexity
         for i in range(j, len(nums)):
             for k in range(i+1, len(nums)):
                 if nums[i]>nums[k]:
