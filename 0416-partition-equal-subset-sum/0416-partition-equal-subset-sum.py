5245416d3d81e58b620e6fce90eff2a0b5ca94ad
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2022-12-08 10:37:01 +0900

    Time: 5071 ms (22.57%), Space: 752.8 MB (5.04%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -7,10 +7,11 @@ class Solution:
         
         half = total / 2
         
+        dp  = {}
         
-        dp  = [0 for j in range(total)]
-        @cache
         def helper(left, right, k):
+            if (left, right, k) in dp:
+                return dp[(left, right, k)]
             if left == right and left!=0 and k==len(nums):
                 return True
             if left > half or right > half:
@@ -19,6 +20,7 @@ class Solution:
                 return False
             a = helper(left+nums[k], right, k+1)
             b = helper(left, right+nums[k], k+1)
+            dp[(left, right, k)] = a or b
             return a or b
                 
         return helper(0,0, 0)
