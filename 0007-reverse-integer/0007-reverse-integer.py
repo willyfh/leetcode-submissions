31d1b41905afc78db83c07bef9a9da2fcbda1a0a
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2023-01-22 15:47:16 +0900

    Time: 73 ms (7.47%), Space: 13.8 MB (96.75%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        mi = -2**31
+        ma = (2**31)-1
+        
+        neg = False
+        if x < 0:
+            neg = True
+            x *= -1
+        t = x
+        ans = 0
+        while t > 0: 
+            m = t % 10
+            ans = 10*ans + m
+            t = t // 10
+        if neg:
+            ans *= -1
+        if ans < mi or ans > ma:
+            ans = 0
+        return ans
