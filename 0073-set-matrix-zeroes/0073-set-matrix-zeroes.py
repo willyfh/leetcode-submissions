6bab4bbcfa24524dd464233dfee3829fa70b7642
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2023-01-25 13:47:13 +0900

    Time: 386 ms (6.51%), Space: 14.9 MB (5.45%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,7 +3,6 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
         first_row_zero = False
         first_col_zero = False
         for i in range(len(matrix)):
@@ -16,16 +15,13 @@ class Solution:
                     if j==0:
                         first_col_zero = True
                         
-                    
         for i in range(1,len(matrix)):
             for j in range(1,len(matrix[0])):
                 if matrix[0][j]==0 or matrix[i][0]==0:
                     matrix[i][j] = 0
-                    
-        for j in range(len(matrix[0])):
-            if first_row_zero:
+        if first_row_zero:         
+            for j in range(len(matrix[0])):
                 matrix[0][j] = 0
-                
-        for i in range(len(matrix)):
-            if first_col_zero:
+        if first_col_zero:       
+            for i in range(len(matrix)):
                 matrix[i][0] = 0
