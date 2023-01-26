d7b10b8693932f0b0e6ec51c54f5f0502aa71cc7
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2023-01-26 18:07:21 +0900

    Time: 128 ms (98.41%), Space: 16.1 MB (38.08%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,50 +3,37 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        i=0
-        j=0
-        flip = [[True for x in range(len(board[0]))] for y in range(len(board))]
-        visited =  [[False for x in range(len(board[0]))] for y in range(len(board))]
         
         
-        def helper(x, y):
-            if x<0 or x>=len(board) or y<0 or y>=len(board[0]):
+        flipped = [[True] * len(board[0]) for i in range(len(board))]
+        
+        def mark(i, j):
+            if i <0 or j <0 or i==len(board) or j==len(board[0]):
                 return
-            if visited[x][y]:
-                return
-            
-            visited[x][y] = True
-            
-            if board[x][y] == "O":
-                
-                flip[x][y] = False
-                helper(x-1, y)
-                helper(x+1, y)
-                helper(x, y-1)
-                helper(x, y+1)
+            if board[i][j] == "#" or board[i][j] == "X":
+                return 
+            board[i][j] = "#"
+            mark(i+1, j)
+            mark(i, j+1)
+            mark(i-1, j)
+            mark(i, j-1)
         
-        for j in range(len(board[0])):
+        for j  in range(len(board[0])):
             if board[0][j] == "O":
-                helper(0,j)
-                
-        for j in range(len(board[0])):
-            if board[-1][j] == "O":
-                helper(len(board)-1,j)
-                
-        for i in range(len(board)):
+                mark(0, j)
+            if board[len(board)-1][j] == "O":
+                mark(len(board)-1, j)
+        
+        for i  in range(len(board)):
             if board[i][0] == "O":
-                helper(i,0)
-                
-        for i in range(len(board)):
-            if board[i][-1] == "O":
-                helper(i,len(board[0])-1)
-                
-                
+                mark(i, 0)
+            if board[i][len(board[0])-1] == "O":
+                mark(i, len(board[0])-1)
+                    
+                    
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if flip[i][j]:
+                if board[i][j] == "#":
+                    board[i][j] = "O"
+                elif board[i][j] == "O":
                     board[i][j] = "X"
-                
-                
-            
-            
