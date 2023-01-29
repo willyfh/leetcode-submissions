41dc8f023e35a7dcab249728803c4aaf9c77cae0
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2023-01-29 14:00:49 +0900

    Time: 59 ms (42.30%), Space: 13.9 MB (17.88%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,48 +1,44 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         
-        
+        def merge(left, right):
+            i = 0
+            j = 0
+            result = []
+            while i < len(left) and j < len(right):
+                if str(left[i])+str(right[j]) > str(right[j])+str(left[i]):
+                    result.append(left[i])
+                    i+=1
+                else:
+                    result.append(right[j])
+                    j+=1
+            
+            while i<len(left):
+                result.append(left[i])
+                i+=1
+            while j<len(right):
+                result.append(right[j])
+                j+=1
+            return result
         
         def mergesort(arr):
-            if len(arr)==1:
-                return
+            if len(arr)<=1:
+                return arr
             
             mid = len(arr)//2
-            
             left = arr[:mid]
             right = arr[mid:]
             
-            mergesort(left)
-            mergesort(right)
-            
-            merge(left, right, arr)
-            
-        def merge(left, right, arr):
-            i=0
-            j=0
-            k=0
-            while i < len(left) and j<len(right):
-                
-                l  = int(str(left[i])+str(right[j]))
-                r = int(str(right[j])+str(left[i]))
-                if l > r:
-                    arr[k] = left[i]
-                    k+=1
-                    i+=1
-                else:
-                    arr[k] = right[j]
-                    k+=1
-                    j+=1
-            while i<len(left):
-                arr[k] = left[i]
-                i+=1
-                k+=1
-                
-            while j<len(right):
-                arr[k] = right[j]
-                k+=1
-                j+=1
-        mergesort(nums)
-        if sum(nums)==0:
+            left = mergesort(left)
+            right = mergesort(right)
+            return merge(left, right)
+        
+        sorted_arr = mergesort(nums)
+        if sorted_arr[0] == 0:
             return "0"
-        return "".join([str(n) for n in nums])
+        ans = ""
+        for i in range(len(sorted_arr)):
+            ans += str(sorted_arr[i])
+        return ans
+
+        
