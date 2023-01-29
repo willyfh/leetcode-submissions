ba6f7cdbe86af690e704176b04ce3fdc7da6174c
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2023-01-29 12:18:36 +0900

    Time: 39 ms (64.14%), Space: 13.8 MB (96.10%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,8 +1,8 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int: 
-        f= 0
-        i = 5
-        while i <= n:
-            f+=n//i
-            i*=5
-        return f
+        ans = 0
+        while n >0:
+            n//=5
+            ans+=n
+        return ans
+    
