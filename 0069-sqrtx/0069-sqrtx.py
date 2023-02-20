9415f92cb69227ec793381f22eecff746f728d36
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2023-02-20 23:05:07 +0900

    Time: 36 ms (79.53%), Space: 13.9 MB (7.59%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -6,10 +6,10 @@ class Solution:
         while l<=r:
             m = l+(r-l)//2
             
-            if m*m == x:
+            if m*m <= x < (m+1)*(m+1):
                 return m
             elif m*m>x:
-                r = m-1
+                r = m
             else:
-                l = m + 1
-        return r
+                l = m
+        return x
