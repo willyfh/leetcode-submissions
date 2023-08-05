18c8153f66111b7a3603340d254df8c7c3532412
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2023-08-06 04:56:46 +0900

    Time: 54 ms (95.82%), Space: 16.3 MB (86.15%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -3,13 +3,13 @@ class Solution:
         k = 1
         d = 1
         for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]:
+            if d < 2 or nums[i] != nums[i-1]:
+                if nums[i] != nums[i-1]:
+                    d = 1
+                elif d < 2:
+                    d += 1
                 nums[k] = nums[i]
-                k+=1
-                d=1
-            else:
-                if d<2:
-                    nums[k] = nums[i]
-                    k+=1
-                    d+=1
+                k += 1
+                
+
         return k
