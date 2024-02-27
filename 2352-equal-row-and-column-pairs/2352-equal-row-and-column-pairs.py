bfd855a307a75c0ba7586c178fe00e945b57a2fa
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-02-27 23:06:45 +0900

    Time: 376 ms (88.65%), Space: 21.4 MB (69.35%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,31 +1,21 @@
-import numpy as np
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         
         row_map = {}
-        col_map = {}
         
-        
-        rows = ['']*len(grid)
-        cols = ['']*len(grid[0])
-        
-        for j in range(len(grid)):
-            for i in range(len(grid[j])):
-                rows[j] += "-"+str(grid[j][i])
-                cols[i] += "-"+str(grid[j][i])
-                
-        for r in rows:
-            if r not in row_map:
-                row_map[r] = 0
-            row_map[r] += 1
-        
-        for c in cols:
-            if c not in col_map:
-                col_map[c] = 0
-            col_map[c] += 1
+        for i in range(len(grid)):
+            t = tuple(grid[i])
+            if t not in row_map:
+                row_map[t] = 0
+            row_map[t]+=1
             
         ans = 0
-        for key in row_map:
-            if key in col_map:
-                ans += (row_map[key]*col_map[key])
+        for j in range(len(grid[0])):
+            cols = []
+            for row in grid:
+                cols.append(row[j])
+            t = tuple(cols)
+            if t in row_map:
+                ans += row_map[t]
+                       
         return ans
