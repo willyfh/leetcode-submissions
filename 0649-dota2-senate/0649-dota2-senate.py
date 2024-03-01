f272c5bf33201d258f4911b213b7a5a1a8cb55c5
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-03-01 12:22:18 +0900

    Time: 1237 ms (7.95%), Space: 16.7 MB (94.49%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,20 +1,16 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
         
-        
         vote = deque()
-
-        n=len(senate)
         
         name_mapping = {'R': 'Radiant', 'D': 'Dire'}
         
         for s in senate:
             vote.append(s)
-        i=0
+            
         while vote:
             curr = vote.popleft()
             vote.append(curr)
-            
 
             temp = []
             while vote and vote[0] == curr:
@@ -25,9 +21,8 @@ class Solution:
                 return name_mapping[curr]
             
             vote.popleft()
-            temp.reverse()
-            for t in temp:
-                vote.appendleft(t)
+            for i in range (len(temp)-1, -1, -1):
+                vote.appendleft(temp[i])
             
             
             
