94bc88152158bc47bb1db38070c6fad359bfa858
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-03-12 22:48:27 +0900

    Time: 654 ms (70.18%), Space: 18.1 MB (43.52%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -0,0 +1,41 @@
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        
+        q = deque()
+        
+        q.append((entrance[0], entrance[1]))
+        
+        visited = set()
+        
+        moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        
+        step = 0 
+        
+        
+        m = len(maze)
+        n = len(maze[0])
+        while len(q) > 0:
+            
+            num_level = len(q)
+            for k in range(num_level):
+                i, j = q.popleft()
+                visited.add((i, j))
+
+                # check exit
+                if (j==0 or j==n-1 or i==0 or i==m-1) and not (i==entrance[0] and j==entrance[1]):
+                    return step
+
+                for x, y in moves:
+                    next_i = i+x
+                    next_j = j+y
+                    if (next_i, next_j) in visited:
+                        continue
+                    # move to empty cell
+                    if next_i>=0 and next_i<m and next_j>=0 and next_j<n and maze[next_i][next_j] == '.':
+                        q.append((next_i, next_j))
+                        visited.add((next_i, next_j))
+                        
+            step += 1
+            
+        return -1
+        
