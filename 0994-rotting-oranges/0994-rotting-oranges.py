131c028d5471ce00d05e981ee9b9cd3fb9373339
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-03-13 23:47:09 +0900

    Time: 47 ms (78.26%), Space: 16.8 MB (19.90%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,60 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        
+        q = deque()
+        m = len(grid)
+        n = len(grid[0])
+        
+        neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        
+        
+        num_fresh = 0
+        visited = set()
+        # find the fresh oranges located next to rotten oranges
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    num_fresh += 1
+                    rotten_adj = False
+                    for neigh in neighbors:
+                        next_i = i + neigh[0]
+                        next_j = j + neigh[1]
+                        if next_i >=0 and next_i <m and next_j>=0 and next_j <n:
+                            if grid[next_i][next_j] == 2:
+                                rotten_adj = True
+                                break
+                    if rotten_adj:
+                        q.append((i, j))
+                        visited.add((i,j))
+                        
+        minute = 0
+        while len(q) > 0:
+            num_level = len(q)
+            minute += 1
+            for k in range(num_level):
+                i, j = q.popleft()
+                grid[i][j] = 2
+
+                num_fresh -= 1
+                
+                for neigh in neighbors:
+                    next_i = i + neigh[0]
+                    next_j = j + neigh[1]
+                    if next_i >=0 and next_i <m and next_j>=0 and next_j <n:
+                        if grid[next_i][next_j] == 1 and (next_i, next_j) not in visited:
+                            q.append((next_i, next_j))
+                            grid[next_i][next_j] = 2
+                            
+        if num_fresh>0:
+            return -1
+        
+        return minute
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
