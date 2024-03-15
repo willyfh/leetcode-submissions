9af9e12111a81fddde7f26c5875f76ff4da70ed6
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-03-15 23:39:23 +0900

    Time: 715 ms (97.34%), Space: 37.3 MB (68.17%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.py
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.py
@@ -1,13 +1,11 @@
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
         
-
         h = []
         
         pairs = list(zip(nums1, nums2))
 
         sorted_pairs = sorted(pairs, key=lambda x: x[1], reverse=True)
-
 
         h = [sorted_pairs[i][0] for i in range(k)]
         heapify(h)
@@ -18,7 +16,6 @@ class Solution:
             s+=sorted_pairs[i][0]
             r = heappushpop(h, sorted_pairs[i][0])
             s-= r
-            ans = max(ans, sorted_pairs[i][1]*s)
-            
+            ans = max(ans, sorted_pairs[i][1]*s)      
         
         return ans
