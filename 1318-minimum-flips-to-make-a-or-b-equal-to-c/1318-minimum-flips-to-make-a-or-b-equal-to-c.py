04818b3134559ff2722906a456589bc6a43dd4d5
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-03-21 21:49:32 +0900

    Time: 37 ms (39.47%), Space: 16.5 MB (59.17%) - LeetHub

---
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        
+        ans = 0
+        for i in range(32):
+            check_a = a & 1
+            check_b = b & 1
+            check_c = c & 1
+            
+            if check_c ==1:
+                if check_a == 0 and check_b == 0:
+                    ans+=1
+            else:
+                ans += check_a + check_b
+            
+            a = a >> 1
+            b = b >> 1
+            c = c >> 1
+        return ans
