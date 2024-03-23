cadc5781cc4bed66f6e33b9afd2c19c117ce3e29
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-03-23 12:20:34 +0900

    Time: 1077 ms (44.94%), Space: 62.9 MB (27.21%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -3,14 +3,11 @@ class Solution:
         points.sort()
         
         ans = 1
-        start = points[0][0]
         end = points[0][1]
         for p in points:
             if p[0] <= end:
-                start = p[0]
                 end = min(end, p[1])
             else:
-                start = p[0]
                 end = p[1]
                 ans+=1
                 
