93b4cd88ec882105419881544eac6790245da65c
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-03-31 23:54:14 +0900

    Time: 105 ms (51.50%), Space: 20.2 MB (40.05%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -2,24 +2,23 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         
         
-        dp = [[0]*len(word2) for i in range(len(word1))]
-        
-        def helper(i, j):
+        dp = [[0]*(len(word2)+1) for i in range(len(word1)+1)]
+    
+    
+        for i in range(len(word1) + 1):
+            dp[i][-1] = len(word1) - i
+
+        for j in range(len(word2) + 1):
+            dp[-1][j] = len(word2) - j
+
             
-            if i ==len(word1):
-                return len(word2) - j
-            elif j==len(word2):
-                return len(word1) - i
             
-            if dp[i][j]>0:
-                return dp[i][j]
-            
-            if word1[i] == word2[j]:
-                dp[i][j] = helper(i+1, j+1)
-                return dp[i][j]
-            else:
-                dp[i][j] = 1 + min(helper(i+1, j+1), helper(i+1, j), helper(i, j+1))
-                return dp[i][j]
-        
-        
-        return helper(0, 0)
+        for i in range(len(word1)-1, -1, -1):
+            for j in range(len(word2)-1, -1, -1):
+                                
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    dp[i][j] = 1 + min(dp[i+1][j+1], dp[i+1][j], dp[i][j+1])
+                    
+        return dp[0][0]
