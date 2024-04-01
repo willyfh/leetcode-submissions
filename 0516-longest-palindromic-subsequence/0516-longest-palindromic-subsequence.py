23c2e92777a1b561c738d70ab0450d9034be4e8c
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-04-01 21:19:33 +0900

    Time: 697 ms (75.38%), Space: 33 MB (68.63%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,22 +1,37 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         
+#         # recursive + memoization
+#         dp = [[0]*len(s) for i in range(len(s))]
+#         def helper(i, j):
+#             if i==j:
+#                 return 1
+#             if i>j:
+#                 return 0
+            
+#             if dp[i][j]>0:
+#                 return dp[i][j]
+#             if s[i] == s[j]:
+#                 dp[i][j] = 2 + helper(i+1, j-1)
+#                 return dp[i][j]
+#             else:
+#                 dp[i][j] = max(helper(i+1, j), helper(i, j-1))
+#                 return dp[i][j]
+            
+#         return helper(0, len(s)-1)   
+        
+        # iterative
         dp = [[0]*len(s) for i in range(len(s))]
-        def helper(i, j):
-            if i==j:
-                return 1
-            if i>j:
-                return 0
+        for i in range(len(s)):
+            dp[i][i] = 1
             
-            if dp[i][j]>0:
-                return dp[i][j]
-            if s[i] == s[j]:
-                dp[i][j] = 2 + helper(i+1, j-1)
-                return dp[i][j]
-            else:
-                dp[i][j] = max(helper(i+1, j), helper(i, j-1))
-                return dp[i][j]
-            
-        return helper(0, len(s)-1)   
-        
-        
+        for j in range(len(s)):
+            for i in range(j-1, -1, -1):
+                if s[i] == s[j]:
+                    dp[i][j] = 2 + dp[i+1][j-1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+                    
+        return dp[0][-1]
+                
+    
