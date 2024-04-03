33563483f1690887225c3912d286526a15edd7e8
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-04-03 23:13:43 +0900

    Time: 279 ms (69.20%), Space: 30.2 MB (74.75%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,11 +1,13 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
+        
+        # # Recursive
         dp = [[-1]*len(t) for i in range(len(s))]
         
         
-        def helper(seq, i, j):
-            if seq == t:
+        def helper(i, j):
+            if j == len(t):
                 return 1
             if i == len(s):
                 return 0
@@ -14,14 +16,24 @@ class Solution:
             out = 0
             if s[i] == t[j]:
                 
-                out = helper(seq+s[i], i+1, j+1)
+                out = helper(i+1, j+1)
                 
-                out+= helper(seq, i+1, j)
+                out+= helper(i+1, j)
                 
             else:
-                out = helper(seq, i+1, j)
+                out = helper(i+1, j)
             
             dp[i][j] = out
             return out
             
-        return helper("", 0, 0)
+        return helper(0, 0)
+
+#         ## ITERATIVE
+    
+#         dp = [[0]*len(t) for i in range(len(s)+1)]
+        
+#         i = 0
+#         j = 0
+        
+        
+        
