2b1b5d2a47c63347711dc67428dbf69824d9c903
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-04-03 23:26:42 +0900

    Time: 242 ms (78.19%), Space: 72.9 MB (56.37%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -3,37 +3,47 @@ class Solution:
         
         
         # # Recursive
-        dp = [[-1]*len(t) for i in range(len(s))]
+#         dp = [[-1]*len(t) for i in range(len(s))]
         
         
-        def helper(i, j):
-            if j == len(t):
-                return 1
-            if i == len(s):
-                return 0
-            if dp[i][j] > -1:
-                return dp[i][j]
-            out = 0
-            if s[i] == t[j]:
+#         def helper(i, j):
+#             if j == len(t):
+#                 return 1
+#             if i == len(s):
+#                 return 0
+#             if dp[i][j] > -1:
+#                 return dp[i][j]
+#             out = 0
+#             if s[i] == t[j]:
                 
-                out = helper(i+1, j+1)
+#                 out = helper(i+1, j+1)
                 
-                out+= helper(i+1, j)
+#                 out+= helper(i+1, j)
                 
-            else:
-                out = helper(i+1, j)
+#             else:
+#                 out = helper(i+1, j)
             
-            dp[i][j] = out
-            return out
+#             dp[i][j] = out
+#             return out
             
-        return helper(0, 0)
+#         return helper(0, 0)
 
-#         ## ITERATIVE
+        ## ITERATIVE
     
-#         dp = [[0]*len(t) for i in range(len(s)+1)]
+        dp = [[0]*(len(t)+1) for i in range(len(s)+1)]
+            
+        for i in range(len(s)+1):
+            dp[i][-1] = 1
         
-#         i = 0
-#         j = 0
-        
+
+        for i in range(len(s)-1, -1, -1):
+            for j in range(len(t)-1, -1, -1):
+                
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i+1][j+1] + dp[i+1][j]
+                    
+                else:
+                    dp[i][j] = dp[i+1][j]
+        return dp[0][0]
         
         
