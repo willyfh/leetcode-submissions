331b66dfa21185d03bb5edecba7202b0a1dee291
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2025-02-16 15:45:18 +0900

    Time: 0 ms (100%), Space: 18.9 MB (48.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,13 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zeros = collections.deque()
+        j = 0
         for i, num in enumerate(nums):
             if num !=0:
-                if len(zeros)==0:
-                    continue
-                j = zeros.popleft()
                 nums[j], nums[i] = nums[i], nums[j]
-                zeros.append(i)
-            else:
-                zeros.append(i)
+                j+=1
